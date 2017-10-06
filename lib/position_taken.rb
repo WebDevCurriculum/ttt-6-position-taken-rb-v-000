8c2fbb37ec76dcd70630059faf82c852c7c6cf90
Charity Youngblood <youngblood.charity@gmail.com>
2017-10-06 00:29:06 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,8 @@
 # code your #position_taken? method here!
 def position_taken?(board,index)
   board[index]
-  # check the board at the index being passed through 
+  # check the value of the board at the given index
+  if board[index] == " "
+  # check the board at the index being passed through
+  # check to see if the board[index] = " "
 end
