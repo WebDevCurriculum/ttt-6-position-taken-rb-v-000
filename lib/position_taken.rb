d5e34fdad0cb8fde21360f73ca360538ca173817
Charity Youngblood <youngblood.charity@gmail.com>
2017-10-06 00:25:47 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,4 @@
 # code your #position_taken? method here!
 def position_taken?(board,index)
   board[index]
-  if board[index] == " "
-    puts "false"
-  elsif board[index] == ""
-    puts "false"
-  end
 end
